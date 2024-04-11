60d9ecbfcd179c506911262cfc7ce6784fef5d93
Shivansh Tariyal <shivanshtariyal@gmail.com>
2024-04-11 12:35:21 +0530

    started on weight tracker

--- app/src/main/java/com/example/hard75tracker/MainActivity.kt
+++ app/src/main/java/com/example/hard75tracker/MainActivity.kt
@@ -39,7 +39,7 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications,R.id.navigation_weight))
         setupActionBarWithNavController(navController, appBarConfiguration)
         ExpandableBottomBarNavigationUI.setupWithNavController(navView,navController)
 
